d19adf057f7dec03020970169d05d33f0131500a
Anush Maharjan <anushmrzan777@gmail.com>
2024-12-09 07:54:30 +0545

    starLoop

---
+++ starpattern.py
@@ -0,0 +1,4 @@
+i = 1 #intialize th value for starting the loop
+while i != int(input("Enter the height:")): #asks for the height of the triangle
+    print("*"*i) #prints "*" throughout the loop while multiplying itself
+    i+=1 #increases the value of loop by one after each loop

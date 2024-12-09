3ebd9b9f45744b3bea5c54628f3e71c6e7d80f34
biraj buddhacharya <birajbuddhacharya@gmail.com>
2024-12-09 16:27:52 +0545

    changes

---
+++ bitesBytes.py
@@ -0,0 +1,8 @@
+from sys import getsizeof
+
+x = 10
+y = 'test'
+z = True
+a = [1, 2, 3]
+
+print(getsizeof(x), getsizeof(y), getsizeof(z), getsizeof(a))
---
+++ forLoops.py
@@ -0,0 +1,4 @@
+mul = 5
+
+for i in range(1, 10): 
+    print(mul, 'x', i, '=', mul*i)

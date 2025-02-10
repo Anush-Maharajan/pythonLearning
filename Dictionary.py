e9a0c0aea0dd7db2d39a3bcb027601af8b4801c1
Anush Maharjan <anushmrzan777@gmail.com>
2025-02-10 08:39:16 +0545

    learning the set datatype

--- Dictionary.py
+++ Dictionary.py
@@ -1,3 +1,7 @@
+"""
+    This is a program for learning the various methods and functions of dictionary!    
+"""
+
 std = {
     "name": "Anush Maharjan",
     "age": 18,
@@ -11,10 +15,18 @@ std.update({"DOB": "2006"})
 print(f"\nstd dictionary after updating value by adding DOB \n{std}")
 
 remove_key = std.pop("age")
-print(f"\nRemoving the age key using pop\n {std}")
+print(f"\nRemoving the age key using pop\n{std}")
 
 l1 = ["abc", "def", "xyz"]
 l2 = ["cba", "fed", "zyx"]
+print(f"Making two lists:\n{l1}\n{l2}\n")
 
+dictionary = dict.fromkeys(l1, l2) #All the l1 elements will have the elements of l2 
+print(f"\nUsing the above list to make a dictionary:\n{dictionary}")
 
-dictionary = dict.setdefault(l1)
+# Adding the keys and values using index
+
+dictionary["abc"] = "cba" # update
+dictionary["ghi"] = "ihg" # add
+
+print(f"\n Adding the keys and values in dictionary using index:\n{dictionary}")
